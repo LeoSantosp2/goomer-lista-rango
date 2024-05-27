759c3dcc1b4be62398a562b14de0e4de810ba66a
Leonardo Santos <leogilberto2010@hotmail.com>
2024-05-27 20:58:01 -0300

    Configurado a estrutura subir um servidor com o express

---
+++ app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import 'dotenv/config';
+
+class App {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {}
+}
+
+export default new App().app;
