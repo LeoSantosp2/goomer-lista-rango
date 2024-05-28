24d74434fdb8970975bcd0437dd4b1dc1e4db3f6
Leonardo Santos <leogilberto2010@hotmail.com>
2024-05-28 10:05:07 -0300

    Feito alterações

--- src/database/run-migrations.ts
+++ src/database/run-migrations.ts
@@ -5,16 +5,22 @@ import knex from '../config/knex';
 
 (async () => {
   try {
-    const createTableFile = fs.readFileSync(
+    const createRestaurantsTable = fs.readFileSync(
       resolve(__dirname, 'migrations', 'create-restaurants-table.sql'),
       {
         encoding: 'utf8',
       },
     );
 
-    await knex.raw(createTableFile);
+    const createProductsTable = fs.readFileSync(
+      resolve(__dirname, 'migrations', 'create-products-table.sql'),
+      {
+        encoding: 'utf8',
+      },
+    );
 
-    console.log('Tabela criada com sucesso.');
+    await knex.raw(createRestaurantsTable);
+    await knex.raw(createProductsTable);
   } catch (err) {
     if (err instanceof Error) {
       console.log('Erro ao criar a tabela:', err.message);
