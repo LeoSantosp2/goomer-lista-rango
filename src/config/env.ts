1ad06ae9b0fa328a7f3e81748bf67068cd796c07
Leonardo Santos <leogilberto2010@hotmail.com>
2024-05-27 23:07:06 -0300

    Adicionado schemas da base de dados

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,11 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
+  DATABASE: z.string(),
+  DATABASE_HOST: z.string(),
+  DATABASE_PORT: z.string(),
+  DATABASE_USERNAME: z.string(),
+  DATABASE_PASSWORD: z.string(),
   PORT: z.string(),
 });
 
