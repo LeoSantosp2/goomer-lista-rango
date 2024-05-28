8ada1ee9b1902d7fe40548996ebe1e89f4cb28c6
Leonardo Santos <leogilberto2010@hotmail.com>
2024-05-27 23:07:18 -0300

    Criado arquivo de configuração do knex

---
+++ src/config/knex.ts
@@ -0,0 +1,25 @@
+import Knex from 'knex';
+
+import env from './env';
+
+const knexfile = {
+  client: 'mysql2',
+  connection: {
+    host: env.DATABASE_HOST,
+    port: env.DATABASE_PORT,
+    database: env.DATABASE,
+    user: env.DATABASE_USERNAME,
+    password: env.DATABASE_PASSWORD,
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+  },
+};
+
+const knex = Knex(knexfile);
+
+export default knex;
