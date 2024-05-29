966d9d163a2f829e19e27146a028831c8a11e77f
Leonardo Santos <leogilberto2010@hotmail.com>
2024-05-28 23:03:42 -0300

    Alterado a variavel de ambiente PORT para API_PORT e adicionado uma variavel de ambiente chamada URL_API

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,7 +6,8 @@ const envSchema = z.object({
   DATABASE_PORT: z.string(),
   DATABASE_USERNAME: z.string(),
   DATABASE_PASSWORD: z.string(),
-  PORT: z.string(),
+  URL_API: z.string(),
+  API_PORT: z.string(),
 });
 
 export default envSchema.parse(process.env);
